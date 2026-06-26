542584076066dca28670a57bfdb8b5255d1e7ffb
Vinit-D-Bisht <bishtvinit88@gmail.com>
2026-06-26 21:05:47 +0530

    fixed migrate errors and added items to display

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Tasks
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        task = request.POST["task"]
+        Tasks.objects.create(title=task)
+        return redirect("/")
+
+    task = Tasks.objects.all()
+    return render(request, "home.html", {"tasks": task})
